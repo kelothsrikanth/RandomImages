fafc0d7fbb16343e5531805af95ed30d8b6aea30
npx <srikanthkeloth@lyrostech.com>
2024-04-03 12:55:45 +0530

    commit

---
+++ Script.js
@@ -0,0 +1,7 @@
+const images=document.querySelectorAll('img');
+const resetButton = document.getElementById('reset');
+const verifyButton = document.getElementById('verify');
+const para=document.getElementById('para');
+const h = document.getElementById('h');
+let ClickedImages=[];
+let state = 1;
